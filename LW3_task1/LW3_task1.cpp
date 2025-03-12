f77629ca3b9126ed5aa4f9554e8a0d5719f0eec7
Vsevolod-pop <vopopenko@gmail.com>
2025-03-13 00:18:54 +0300

    Доработал проверку

--- LW3_task1/LW3_task1.cpp
+++ LW3_task1/LW3_task1.cpp
@@ -13,13 +13,13 @@ int main()
     string stroka;
     cout << "Введите количество элементов в массиве - n (n - натуральное число) - ";
     getline(cin, stroka);
-    int size;
+    int n;
     while (true)
     {
         try
         {
-            size = stoi(stroka);
-            if ((to_string(size) == stroka) and (size > 0))
+            n = stoi(stroka);
+            if ((to_string(n) == stroka) and (n > 0))
             {
                 break;
             }
@@ -35,47 +35,120 @@ int main()
         }
     }
     srand(time(NULL));
-    float* arr = new float[size];
-    for (int i = 0; i < size; i++)
+    float* arr = new float[n];
+    for (int i = 0; i < n; i++)
     {
-        *(arr + i) = (double)(rand()) / RAND_MAX * (1000 + 1000) - 1000;
+        *(arr + i) = (double)(rand()) / RAND_MAX * (10 + 10) - 10;
     }
     double C;
     cout << "Введите число C - ";
     getline(cin, stroka);
     while (true)
     {
+        if (stroka.find(".") != string::npos)
+            stroka.replace(stroka.find("."), 1, ",");
         try
         {
-            C = stof(stroka);
-            stringstream C1;
-            C1 << C;
-            if (stroka == "0")
-            {
-                break;
-            }
-            if ((C1.str() == stroka.replace(stroka.find(","), 1, ".")) and C != 0)
-            {
-                break;
-            }
-            else
+            if (stroka == "")
             {
                 throw logic_error("Число введено некорректно");
             }
+            if (stroka == "0")
+            {
+                C = 0;
+                break;
+            }
+            if ((stroka[1] != ',' and stroka[0] == '0') or (stroka[0] == '-' and stroka[1] == '0' and stroka[2] != ','))
+            {
+                throw logic_error("Число введено некорректно");
+            }
+            int flag = 0;
+            for (int i = 0; i < stroka.length(); i++)
+            {
+                if (stroka[i] == '-' and i == 0)
+                {
+                    continue;
+                }
+                if (stroka[i] == ',')
+                {
+                    flag++;
+                    if (flag > 1)
+                    {
+                        throw logic_error("Число введено некорректно");
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                if (!isdigit(stroka[i]))
+                {
+                    throw logic_error("Число введено некорректно");
+                }
+            }
+            C = stof(stroka);
+            break;
         }
         catch (...)
         {
-            cout << "Число введено некорректно. Введите количество элементов в массиве - n (n - натуральное число) - " ;
+            cout << "Число введено некорректно. Введите количество элементов в массиве - n (n - натуральное число) - ";
             getline(cin, stroka);
         }
     }
-    
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << " элемент исходного массива равен: " << *(arr + i) << endl;
+    }
     int countC = 0;
     int summ = 0;
-
-
-    for (int i = 0; i < size; i++)
+    int schetchik = 0;
+    float* arr_new = new float[n];
+    float max = -99999999.0;
+    int min_i = 0;
+    for (int i = 0; i < n; i++)
     {
-        cout << i + 1 << " элемент массива равен: " << *(arr + i) << endl;
+        if (i == 0)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (arr[j] > max)
+                {
+                    max = arr[j];
+                }
+                if (arr[j] < 0)
+                {
+                    min_i = j;
+                }
+            }
+        }
+        if (arr[i] < C)
+        {
+            countC++;
+        }
+        if (i > min_i)
+        {
+            summ += int(arr[i]);
+        }
     }
+    int j = n-1;
+    for (int i = 0; i < n; i++)
+    {
+        if ((max * 0.8 < arr[i]) and (arr[i] < max))
+        {
+            arr_new[schetchik] = arr[i];
+            schetchik++;
+        }
+        else
+        {
+            arr_new[j] = arr[i];
+            j--;
+        }
+    }
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << i + 1 << " элемент нового массива равен: " << arr_new[i] << endl;
+    }
+    cout << endl;
+    cout << "Количество элементов массива меньших С - " << countC << endl << "Сумма целых частей элементов массива, расположенных после последнего отрицательного элемента - " << summ << endl;
 }
